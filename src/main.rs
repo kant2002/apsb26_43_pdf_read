4b8a356db2d6e994d331f20ada37e86786c816d9
Andrii Kurdiumov <kant2002@gmail.com>
2026-05-02 23:20:54 +0200

    Add extraction of payload 2

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use base64::prelude::*;
 use std::fs;
 use std::env;
 
-#[cfg(not(feature = "async"))]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = env::args().collect();
-
+    let file_name = &args[1];
+    println!("Extracting payload from {}", file_name);
     // Load existing PDF
-    let mut doc = Document::load(args[0])?;
-    
+    let doc = Document::load(file_name)?;
 
     for (key, val) in doc.objects.iter() {
         if key.0 == 7 {
@@ -48,7 +47,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("stream key: {} - {}", key.0, key.1);
             println!("enum_variant: {}", val.enum_variant());
             match val.enum_variant() {
-                "Array" => println!("enum_variant: {:?}", val.as_array()),
+                "Dictionary" => {
+                    let second_payload_dict =val.as_dict()?;
+                    let names = second_payload_dict.iter().nth(0).unwrap().1;
+                    let dict_with_code = names.as_array()?.iter().nth_back(0).unwrap().as_dict()?;
+                    fs::write("payload2.js", dict_with_code.get(b"JS")?.as_str()?)?;
+                }
                 _ => match val.type_name() {
                     Ok(r) => println!("type_name: {:?}", str::from_utf8(r)),
                     Err(err) => println!("type_name: {}", err)
@@ -57,12 +61,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         
     }
-    Ok(())
-}
-
-#[cfg(feature = "async")]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // For async feature, you need to use tokio runtime
-    println!("This example requires the async feature to be disabled");
     Ok(())
 }
